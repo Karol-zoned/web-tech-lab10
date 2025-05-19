3b27ea0968700b19aa4df2f843028c90277495af
Karol-zoned <kargul.karolina47@gmail.com>
2025-05-19 14:58:06 +0200

    Fix file tracking

---
+++ labki10-github/src/main.js
@@ -0,0 +1,6 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1 class="text-2xl font-bold text-primary">Hello World!</h1>
+  <button id="count" class="bg-blue-300 p-4">Count</button>
+`
